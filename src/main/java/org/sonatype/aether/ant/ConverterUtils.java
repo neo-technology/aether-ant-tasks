df8791d33295593ad39b951809949797d7811ecc
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2011-05-25 18:57:18 +0200

    o Added license headers

--- src/main/java/org/sonatype/aether/ant/ConverterUtils.java
+++ src/main/java/org/sonatype/aether/ant/ConverterUtils.java
@@ -1,5 +1,17 @@
 package org.sonatype.aether.ant;
 
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -66,7 +78,7 @@ public class ConverterUtils
     }
 
     /**
-     * Converts the given ant repository type to an Aether repository insance with authentication and proxy filled in
+     * Converts the given ant repository type to an Aether repository instance with authentication and proxy filled in
      * via the sessions' selectors.
      */
     public static org.sonatype.aether.repository.RemoteRepository toDistRepository( RemoteRepository repo,
